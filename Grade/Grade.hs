c93d9c74d35457504b8ab7095cd6000a57eaa658
jheave <jimenezjheave123@gmail.com>
2022-06-05 23:07:00 +0800

    fix erro

---
+++ Grade/Grade.hs
@@ -0,0 +1,15 @@
+module GradeBook (getGrade) where
+
+getGrade :: Double -> Double -> Double -> Char
+getGrade x y z
+  | ave >= 90 = a
+  | ave >= 80 = b
+  | ave >= 70 = c
+  | ave >= 60 = d
+  | ave < 60 = f
+  where ave = sum (x, y, z) / 3
+        a = 'A'
+        b = 'B'
+        c = 'C'
+        d = 'D'
+        f = 'F'
