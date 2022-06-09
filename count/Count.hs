07ee6ac9e79658bae746e554107f98982522316b
jheave <jimenezjheave123@gmail.com>
2022-06-09 18:59:42 +0800

    add count

---
+++ count/Count.hs
@@ -0,0 +1,8 @@
+module Kata where
+
+-- Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+
+--If the input is an empty array or is null, return an empty array.
+
+countPositivesSumNegatives :: Maybe [Int] -> [Int]
+countPositivesSumNegatives xs = [length [x | x <- xs, x > 0], sum [x | x <- xs, x < 0]]
