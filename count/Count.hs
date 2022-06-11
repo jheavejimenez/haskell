af94419f817ccd8a82c1603a2f0b19b194977e58
jheave <jimenezjheave123@gmail.com>
2022-06-11 23:17:46 +0800

    fix solution

--- count/Count.hs
+++ count/Count.hs
@@ -1,8 +1,9 @@
 module Kata where
-
+import Data.List ( transpose )
 -- Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
-
 --If the input is an empty array or is null, return an empty array.
 
+
 countPositivesSumNegatives :: Maybe [Int] -> [Int]
-countPositivesSumNegatives xs = [length [x | x <- xs, if x > 0], sum [x | x <- xs, if x < 0]]
+countPositivesSumNegatives Nothing = []
+countPositivesSumNegatives (Just input) = map sum . transpose $ [if xs > 0 then [1, 0] else [0, xs] | xs <- input]
