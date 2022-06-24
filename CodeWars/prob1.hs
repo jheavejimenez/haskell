3e53ad093a3a0299a7d12d8b815ef53a199de394
jheave <jimenezjheave123@gmail.com>
2022-06-24 17:26:08 +0800

    added guards solution

--- CodeWars/prob1.hs
+++ CodeWars/prob1.hs
@@ -4,7 +4,14 @@ evenOrOdd :: Integral a => a -> [Char]
 evenOrOdd n = if even n then "Even" else "Odd"
 
 -- Recursive solution
+
 evenOrOddRecursive :: Integral a => a -> [Char]
 evenOrOddRecursive 0 = "Even" -- base case
 evenOrOddRecursive 1 = "Odd" -- base case
 evenOrOddRecursive n = evenOrOddRecursive (n `mod` 2) -- recursive call
+
+-- Gaurds solution
+evenOrOddGaurd :: Integral a => a -> [Char]
+evenOrOddGaurd n
+    | even n = "Even"
+    | otherwise = "Odd"
