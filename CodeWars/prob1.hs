6abb2fc111f14107861012389c3589cb3643424a
jheave <jimenezjheave123@gmail.com>
2022-06-24 17:19:30 +0800

    recursive solution

--- CodeWars/prob1.hs
+++ CodeWars/prob1.hs
@@ -2,3 +2,9 @@
 
 evenOrOdd :: Integral a => a -> [Char]
 evenOrOdd n = if even n then "Even" else "Odd"
+
+-- Recursive solution
+evenOrOddRecursive :: Integral a => a -> [Char]
+evenOrOddRecursive 0 = "Even" -- base case
+evenOrOddRecursive 1 = "Odd" -- base case
+evenOrOddRecursive n = evenOrOddRecursive (n `mod` 2) -- recursive call
